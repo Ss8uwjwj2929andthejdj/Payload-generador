fc027dcccf47502cb0f635ef39647d87891ceb9d
Ss8uwjwj2929andthejdj <58967862+Ss8uwjwj2929andthejdj@users.noreply.github.com>
2019-12-26 22:09:12 -0600

    Create Payload.sh

---
+++ Payload.sh
@@ -0,0 +1,111 @@
+#!bin/bash
+#by :dexter1999
+echo -e "\e[1;36m"
+sleep 1
+echo   "  /\                         |\**/|"
+echo   " /  \                        \ == / "
+echo   " |  |                         |  | "
+echo   "/ == \                        \  / "
+echo   "|/**\|                         \/  "
+echo ""
+sleep 1
+
+echo "▁ ▂ ▄ ▅ ▆ ▇ █ հԵԵթ íղյҽcԵօɾ █ ▇ ▆ ▅ ▄ ▂ ▁"
+echo ""
+sleep 1
+echo "•.¸♡ gєиєяα∂οя ∂є ραγℓοα∂ 3.0  ♡¸.•"
+echo ""
+sleep 1
+echo "╔═╗╔═╗╔═╗╔══╗╔══╗╔═╗╔═╗  ╔╗╔╗╔══╗╔═╗╔╦╗╔═╗╔══╗  ╔══╗╔══╗
+║╔╝║╬║║╦╝║╔╗║╚╗╗║║║║║╬║  ║╚╝║║╔╗║║╔╝║╔╝║╦╝╚╗╗║  ║╔╗║╚╗╗║
+║╚╗║╗╣║╩╗║╠╣║╔╩╝║║║║║╗╣  ║╔╗║║╠╣║║╚╗║╚╗║╩╗╔╩╝║  ║╠╣║╔╩╝║
+╚═╝╚╩╝╚═╝╚╝╚╝╚══╝╚═╝╚╩╝  ╚╝╚╝╚╝╚╝╚═╝╚╩╝╚═╝╚══╝  ╚╝╚╝╚══╝
+───────────────────────  ─────────────────────  ──────── "
+echo ""
+sleep 1
+echo "∂є∂ιϲα∂ο α мι gяυρο"
+sleep 2
+echo "B¡ทℯℛℴ₷ ðℯ ☪ℴℛα☡óท ƴ ℳα₷"
+echo ""
+echo "εʝεɱρℓσ ∂ε ɦσรƭ"
+echo "facebook.com"
+echo ""
+sleep 1
+echo -ne "\e[1;36mρσɳ ƭµ ɦσรƭ: \e[1;33m"
+read host
+echo ""
+
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "CONNECT [host_port]GET $host HTTP/1/1[crlf] [protocol][crlf] [protocol][crlf][crlf]"
+
+echo ""
+echo ""
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "GET $host HTTP/1.0[crlf][cr|f][sp|it_delay][method] $host@[host_port][protocol][crlf][crlf]"
+
+echo ""
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "CONNECT [host_port][split]GET $host HTTP/1.1[lf][lf]"
+
+echo ""
+
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "GET $host HTTP/1.0[crlf][cr|f][sp|it_delay][method] $host@[host_port][protocol][crlf][crlf]"
+echo ""
+
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "GET $host HTTP/1.1[crlf] ٌُْْ[crlf][crlf]CONNECT [host_port][delay_split][crlf]"
+
+echo ""
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "CONNECT  $host@[host_port] [protocol][split]HEAD $host HTTP/1.1[crlf]Host:$host[crlf][crlf]"
+
+echo ""
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "OPTIONS $host HTTP/1.1[crlf]Host: $host[crlf][crlf]CONNECT [host_port] [protocol][crlf][crlf]"
+
+echo ""
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "CONNECT [host_port]@$host [protocol][crlf][instant_split]GET $host HTTP/1.1[crlf]Host: $host[crlf][crlf]"
+echo ""
+echo ""echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "CONNECT [host_port] [protocol][crlf]Host: $host[crlf][crlf]"
+
+echo ""
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+sleep 2
+echo ""
+echo "TRACE $host HTTP/1.0 Host:$host Connection: Keep-Alive [crlf]"
+
+
+echo ""
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+echo ""
+sleep 2
+echo "HEAD HTTP/[crlf] [crlf]Host: $host[crlf] [crlf]"
+
+echo ""
+echo "★彡ραγℓοα∂ gєиєяα∂ο彡★"
+echo ""
+sleep 2
+echo "CONNECT $host@[host_port] HTTP/1.1[crlf][crlf]GET $host [protocol][crlf]Host: $host[crlf]X-Forwarded-For: $host[crlf]Connecti                   >
+
+echo ""
